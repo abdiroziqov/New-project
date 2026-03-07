f34e699c5f8150f2e43816cc1394a21918fc08de
ilhomabdiroziqov <i.abdiroziqov@uic.group>
2026-03-07 10:16:34 +0500

    devtools: { enabled: false },

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
   css: ['~/assets/css/tailwind.css'],
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   typescript: {
     strict: true
   },
